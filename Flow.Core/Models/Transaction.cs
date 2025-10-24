9aadc7e0b09d7ce84405cc8d7a8140977ccfa136
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2025-10-24 17:34:54 -0300

    feat: adding Amount property to Transaction model

--- Flow.Core/Models/Transaction.cs
+++ Flow.Core/Models/Transaction.cs
@@ -12,6 +12,8 @@ public class Transaction
 
     public ETransactionType Type { get; set; } = ETransactionType.Withdraw;
 
+    public decimal Amount { get; set; }
+
     public long CategoryId { get; set; }
     public Category Category { get; set; } = null!;
 
